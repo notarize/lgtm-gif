bc51efcf2b346e5775aa1565d417b37b6c6b3715
Kenny Tsui <kenny.tsui@notarize.com>
2019-10-17 17:46:20 -0400

    giphy stuff

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } 
+  try {
+    const inputs = {
+      token: core.getInput('repo-token', {required: true}),
+    }
+    const GIPHY_API_KEY = process.env.GIPHY_API_KEY
+    core.info("Got Token");
+    core.info(`${process.env.TEST_ENV}`);
+    let fetch = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=lgtm`);
+    let giphies = await(fetch.json())
+    core.info(giphies);
+  }
   catch (error) {
     core.setFailed(error.message);
   }
