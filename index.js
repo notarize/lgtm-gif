56e17ad6403df4cb7f9169ffbdaa1e65e412e71d
Kenny Tsui <kenny.tsui@notarize.com>
2019-10-17 18:06:12 -0400

    to stringify

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function run() {
     core.info(`${process.env.TEST_ENV}`);
     const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=lgtm`);
     const giphies = await(response.json())
-    core.info(giphies.toString());
+    core.info(JSON.stringify(giphies));
   }
   catch (error) {
     core.setFailed(error.message);
