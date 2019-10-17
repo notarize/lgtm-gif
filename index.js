da4a4807399e4ab97c2eadccc4b7509e14de9580
Kenny Tsui <kenny.tsui@notarize.com>
2019-10-17 18:17:57 -0400

    get data

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function run() {
     core.info("Got Token");
     core.info(`${process.env.TEST_ENV}`);
     const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=lgtm`);
-    const giphies = await(response.json())['data'];
+    const giphies = await(response.json()['data']);
     core.info(JSON.stringify(giphies));
     core.info(`Length: ${giphies.length}`);
     const randomIndex = parseInt(Math.random() * giphies.length);
