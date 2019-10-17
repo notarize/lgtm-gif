e99d9111440515922534d04dc582b4fe477e55f9
Kenny Tsui <kenny.tsui@notarize.com>
2019-10-17 17:54:45 -0400

    use other var name

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ async function run() {
       token: core.getInput('repo-token', {required: true}),
     }
     const GIPHY_API_KEY = process.env.GIPHY_API_KEY
+    if (!GIPHY_API_KEY) {
+      core.setFailed("Missing GIPHY_API_KEY");
+      return;
+    }
     core.info("Got Token");
     core.info(`${process.env.TEST_ENV}`);
-    let fetch = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=lgtm`);
-    let giphies = await(fetch.json())
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=lgtm`);
+    const giphies = await(response.json())
     core.info(giphies);
   }
   catch (error) {
